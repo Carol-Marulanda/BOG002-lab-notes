d542d71083dba0f091d2821a9f2ea11c3c82c6e3
Carol Marulanda <clmarulandac@gmail.com>
2021-07-05 20:30:25 -0500

    Instalacion React, creacion de componentes y eslint ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Landing from './components/landing';
+
+import './App.css';
+
+function App() {
+  return (
+   <div className='container'>
+     <h1>todo ok</h1>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Landing = () => {
+
+    return (
+        <div className="Landing">
+        <h1>Hello</h1>
+        </div>
+        );
+}
+
+export default Landing;
